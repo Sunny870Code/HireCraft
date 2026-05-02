d2eeb1f571fc19931dca273a03c03f909fd904f7
user.email :"sunnypawar870@gmail.com <sunnypawar870@gmail.com>
2026-05-02 22:08:36 +0530

    add CORS middleware to server for handling cross-origin requests

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,14 @@ const app = require("./src/app")
 const connectDB = require("./src/config/database")
 const {generateInterviewReport} = require("./src/services/ai.service")
 const {resume ,selfDescription, jobDescription } = require("./src/services/temp");
+const cors = require('cors');
 
+app.use(cors({
+    origin: "https://hire-craft-five.vercel.app", // Your exact Vercel URL from the logs
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 connectDB();
 
