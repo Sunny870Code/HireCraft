39b9d47e757c503bf522f151c40dd54e3fff2c6c
user.email :"sunnypawar870@gmail.com <sunnypawar870@gmail.com>
2026-05-02 23:01:59 +0530

    fix: correct error handling in login and registration functions and ensure loading state is managed properly

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -13,59 +13,63 @@ export function useAuth() {
         setLoading(true)
         try {
             const data = await login({ email, password });
-            setuser(data.user);
-        } catch (err) {
-            console.log(err)
-            return null
-        } finally {
-            setLoading(false);
-        }
-    }
-
-
-    const handleRegister = async ({ username, email, password }) => {
-        setLoading(true)
-        try {
-            const data = await register({ username, email, password });
             if (data && data.user) {
                 setuser(data.user);
                 return data;
             }
-        } catch (err) {
-           console.error("Registration failed:", error.data?.data || error.message);
-        return null;
-        } finally {
+            return null;
+            } catch (err) {
+                 console.error("Login error:", err)
+                return null
+            } finally {
+                setLoading(false);
+            }
+        }
+
+
+    const handleRegister = async ({ username, email, password }) => {
+            setLoading(true)
+            try {
+                const data = await register({ username, email, password });
+                if (data && data.user) {
+                    setuser(data.user);
+                    return data;
+                }
+            } catch (err) {
+                console.error("Registration failed:", error.data?.data || error.message);
+                return null;
+            } finally {
+                setLoading(false);
+            }
+
+        }
+
+        const handleLogout = async () => {
+            setLoading(true)
+            const data = await logout();
+            setuser(null);
             setLoading(false);
         }
 
+        // useEffect(() => {
+
+        //     const getAndSetUser = async () => {
+        //         try {
+        //             const data = await getMe();
+        //             if (data && data.user) {
+        //                 setuser(data.user);
+        //             }
+        //         } catch (err) {
+        //             // 401 is normal if not logged in; just keep user as null
+        //             console.warn("User not authenticated");
+        //             setuser(null);
+        //         } finally {
+        //             setLoading(false); // Stop the spinner regardless
+        //         }
+        //     };
+
+        //     getAndSetUser();
+        // }, [])
+
+        return { user, loading, handleLogin, handleRegister, handleLogout }
     }
-
-    const handleLogout = async () => {
-        setLoading(true)
-        const data = await logout();
-        setuser(null);
-        setLoading(false);
-    }
-
-    // useEffect(() => {
-
-    //     const getAndSetUser = async () => {
-    //         try {
-    //             const data = await getMe();
-    //             if (data && data.user) {
-    //                 setuser(data.user);
-    //             }
-    //         } catch (err) {
-    //             // 401 is normal if not logged in; just keep user as null
-    //             console.warn("User not authenticated");
-    //             setuser(null);
-    //         } finally {
-    //             setLoading(false); // Stop the spinner regardless
-    //         }
-    //     };
-
-    //     getAndSetUser();
-    // }, [])
-
-    return { user, loading, handleLogin, handleRegister, handleLogout }
-}
